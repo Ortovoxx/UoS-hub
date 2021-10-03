e2b98dc724ade089897db4757c8810577836e63b
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-10-03 18:13:53 +0100

    Binds to port

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ module.exports = {
     DATABASE_URL: 'mongodb+srv://ortovox-mongo-dev:<PASSWORD>@rh-cluster-0.rzlzp.mongodb.net/RH-Cluster-0?retryWrites=true&w=majority',
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
     NODE_ENV: 'development',
-    PORT: 5000,
+    PORT: process.env.PORT || 5000,
 
     Email_From: 'UmadAhmad1928@gmail.com',
     Sendgrid_Password: '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',
